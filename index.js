121ad9530b6f72ceaf7cf58be6ad6055fd063e76
Rachida Amourak <rachidaamourak6@gmail.com>
2025-04-28 08:30:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const weatherForm=document.querySelector(".WeatherForm");
 const cityInput=document.querySelector(".cityInput");
 const card=document.querySelector(".card");
-const apikey="your api key"
+const apikey="0ec502880fdcf7fb726b2aa98c2d15c8"
 weatherForm.addEventListener("submit", async event=>{
 event.preventDefault();
 
